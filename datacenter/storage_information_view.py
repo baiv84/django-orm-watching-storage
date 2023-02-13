34e0de87fc026ce84ebcdfd2371cbb38d4fd2bee
baiv84 <iambabintsev@gmail.com>
2023-02-14 01:05:00 +0700

    Finished steps 1-12

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,22 @@
+from basic.timefuncs import get_duration
+from basic.timefuncs import format_duration
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    not_leaved_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in not_leaved_visits:
+        owner_name = visit.passcard.owner_name
+        entered_at = visit.entered_at
+        duration = get_duration(visit)
+        print_duration = format_duration(duration)
+        one_visit = dict(who_entered=owner_name, entered_at=entered_at, duration=print_duration)    
+        non_closed_visits.append(one_visit)
+        
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
